784e4b9b053203526e1adcf646253922e0122894
kashmithnisakya <kashmithnisakya@gmail.com>
2024-08-12 20:52:23 +0530

    nev bar ready

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,29 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import Nav from "./Nav";
+
+
 const Header = () => {
   return (
-    <header>
-      <h1>My React App</h1>
+    <header className="py-8 xl:py-12 text-white">
+        <div className="container mx-auto flex justify-between items-center">
+
+            <Link href="/">
+                <h1 className="text-4xl font-semibold">
+                    Kashmith<span className="text-accent">.</span>
+                </h1>
+            </Link>
+        
+            <div className="hidden xl:flex item-center gap-8">
+                <Nav />
+
+                <Link href="/contact">
+                    <Button>Hire me</Button>
+                </Link>
+            </div>
+
+            <div className="xl:hidden">mobile nav</div>
+        </div>
     </header>
   );
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const link = [
+    {
+        name: "Home",
+        path: "/",
+    },
+    {
+        name: "Services",
+        path: "/services",
+    },
+    {
+        name: "About",
+        path: "/about",
+    },
+];
+
+const Nav = () => {
+    const pathname = usePathname();
+    console.log(pathname);
+    return (
+        <nav className="flex gap-8">
+            {link.map((link, index) => {
+                return (
+                    <Link 
+                        href={link.path}
+                        key={index}  
+                        className={`${
+                            link.path === pathname && "text-accent border-b-2 border-accent"} 
+                            capitalize font-medium hover:text-accent transition-all`}>
+                                {link.name}
+                    </Link>
+                );
+            })}
+        </nav>
+    );
+    }
+
+export default Nav;
