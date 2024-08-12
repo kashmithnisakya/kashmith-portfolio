b16e81b08c60e11bde72b27b0cfd6ae650f6d03f
kashmithnisakya <kashmithnisakya@gmail.com>
2024-08-13 02:17:56 +0530

    home page is ready

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,26 +1,35 @@
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
+import Socials from "@/components/Socials";
+import Photo from "@/components/Photo";
 
 const Home = () => {
     return (
-        <section className="py-16">
-            <div className="container mx-auto flex flex-col items-center gap-8">
-                <span className="text-xl">Machine Learning Engineer</span>
-                <h1 className="text-4xl font-semibold text-center">
-                    Hi, I'm Kashmith Nisakya<span className="text-accent">.</span>
-                </h1>
-                <p className="text-center text-lg text-white">
-                   Passionate about solving real-world problems with data.
-                </p>
-
-                <div className="flex gap-4">
-                    {/* add photo */}
-                    <img src="/kashmith.jpg" alt="Kashmith" className="w-48 h-48 rounded-full" />
+        <section className="h-full">
+            <div className="container mx-auto h-full">
+                <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+                    <div className="text-center xl:text-left">
+                        <span className="text-xl">Machine Learning Engineer</span>
+                        <h1 className="h1 mb-6">
+                            Hello, I'm <br /><span className="text-accent">Kashmith Nisakya</span>
+                        </h1>
+                            <p className="max-w-[500px] mb-9 text-white/80">
+                                Passionate about solving real-world problems with data.
+                            </p>
+                            <div className="flex flex-col xl:flex-row items-center gap-8">
+                                <Button variant="outline" size="large" as="a" href="/resume.pdf" download className="uppercase flex items-center gap-2">
+                                    <FiDownload className="text-xl"/>
+                                    Download Resume
+                                </Button>
+                            <div className="mb-8 xl:mb-0">
+                            <Socials containerStyles="flex gap-6" iconStyles="w-9 h-9 border-accent rounded flex justify-center item-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <Photo /> 
+                    </div>
                 </div>
-                <Button variant="primary" size="large" as="a" href="/resume.pdf" download>
-                    <FiDownload className="mr-2" />
-                    Download Resume
-                </Button>
             </div>
         </section>
     );
