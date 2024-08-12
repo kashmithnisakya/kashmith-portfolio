5c56d6623ac9ec1dc471dee626eab2324581815a
kashmithnisakya <kashmithnisakya@gmail.com>
2024-08-12 22:39:05 +0530

    Mobile Nev is ready

---
+++ src/components/MobileNav.jsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import { CiMenuFries } from 'react-icons/ci';
+
+
+
+const links = [
+    {
+        name: "Home",
+        path: "/",
+    },
+    {
+        name: "Projects",
+        path: "/projects",
+    },
+    {
+        name: "Resume",
+        path: "/resume",
+    },
+    {
+        name: "Work",
+        path: "/work",
+    },
+    {
+        name: "Contact",
+        path: "/contact",
+    },
+];
+
+const MobileNav = () => {
+    const pathname = usePathname();
+
+    return <Sheet>
+        <SheetTrigger>
+            <CiMenuFries className="text-4xl" />
+        </SheetTrigger>
+        <SheetContent>
+            <nav className="flex flex-col gap-4">
+                {links.map((link, index) => {
+                    return (
+                        <Link
+                            href={link.path}
+                            key={index}
+                            className={`${
+                                link.path === pathname && "text-accent border-b-2 border-accent"
+                            } capitalize font-medium hover:text-accent transition-all`}
+                        >
+                            {link.name}
+                        </Link>
+                    );
+                })}
+            </nav>
+        </SheetContent>
+    </Sheet>;
+    }
+
+export default MobileNav;
