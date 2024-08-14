5b466172254e8114ac34dab888243b752a984973
kashmithnisakya <kashmithnisakya@gmail.com>
2024-08-14 20:31:17 +0530

    ready to marge

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -1,8 +1,8 @@
 const Resume = () => {
     return (
-        <div>
-            <h1>Resume</h1>
-            <p>My resume will go here.</p>
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-3xl font-bold">Resume</h1>
+            <p className="text-lg">This is the resume page</p>
         </div>
     );
 }
